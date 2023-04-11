c15663f7b2c7537faeb2539f48cfd7e47a7e4eb2
Killathon <huelva21@gmail.com>
2023-04-11 12:31:28 -0700

    First error-free version with basic tests, ATR separation

---
+++ Source/v1.4/Components/ThingComps/CompPawnSpawnerPermanentHostile.cs
@@ -0,0 +1,37 @@
+﻿using Verse;
+using RimWorld;
+
+namespace ATReforged
+{
+    public class CompPawnSpawnerPermanentHostile : CompPawnSpawner
+    {
+        public new CompProperties_PawnSpawnerPermanentHostile Props
+        {
+            get
+            {
+                return props as CompProperties_PawnSpawnerPermanentHostile;
+            }
+        }
+
+        // Take the pawn that was generated and spawned from the base class and add a few features to it.
+        public override Pawn SpawnPawn()
+        {
+            Pawn pawn = base.SpawnPawn();
+
+            pawn.SetFactionDirect(Faction.OfAncientsHostile);
+            pawn.mindState.mentalStateHandler.TryStartMentalState(ATR_MentalStateDefOf.ATR_MentalState_Exterminator, transitionSilently: true);
+            
+            if (Props.hediffDefToSpawnWith != null)
+            {
+                Hediff hediff = HediffMaker.MakeHediff(Props.hediffDefToSpawnWith, pawn, null);
+                if (Props.hediffSeverity != -1f)
+                {
+                    hediff.Severity = Props.hediffSeverity;
+                }
+                pawn.health.AddHediff(hediff, null, null);
+            }
+
+            return pawn;
+        }
+    }
+}
